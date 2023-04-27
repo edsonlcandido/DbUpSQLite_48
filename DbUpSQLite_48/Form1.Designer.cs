3b26fcbefff26d6240d4e26976ddaf058ee1d5bd
edsonlcandido <edson.luiz.candido@gmail.com>
2023-04-26 23:13:01 -0300

    Adicionar arquivos de projeto.

---
+++ DbUpSQLite_48/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace DbUpSQLite_48
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonOk = new System.Windows.Forms.Button();
+            this.buttonAddDatabase = new System.Windows.Forms.Button();
+            this.textBoxDatabasePath = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonOk
+            // 
+            this.buttonOk.Location = new System.Drawing.Point(373, 69);
+            this.buttonOk.Name = "buttonOk";
+            this.buttonOk.Size = new System.Drawing.Size(116, 23);
+            this.buttonOk.TabIndex = 0;
+            this.buttonOk.Text = "Execute Migration";
+            this.buttonOk.UseVisualStyleBackColor = true;
+            this.buttonOk.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // buttonAddDatabase
+            // 
+            this.buttonAddDatabase.Location = new System.Drawing.Point(12, 12);
+            this.buttonAddDatabase.Name = "buttonAddDatabase";
+            this.buttonAddDatabase.Size = new System.Drawing.Size(112, 23);
+            this.buttonAddDatabase.TabIndex = 1;
+            this.buttonAddDatabase.Text = "Database->";
+            this.buttonAddDatabase.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.buttonAddDatabase.UseVisualStyleBackColor = true;
+            this.buttonAddDatabase.Click += new System.EventHandler(this.buttonAddDatabase_Click);
+            // 
+            // textBoxDatabasePath
+            // 
+            this.textBoxDatabasePath.Location = new System.Drawing.Point(130, 14);
+            this.textBoxDatabasePath.Name = "textBoxDatabasePath";
+            this.textBoxDatabasePath.Size = new System.Drawing.Size(359, 20);
+            this.textBoxDatabasePath.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(130, 43);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(359, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 41);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(112, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Migrations folder ->";
+            this.button3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(499, 102);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBoxDatabasePath);
+            this.Controls.Add(this.buttonAddDatabase);
+            this.Controls.Add(this.buttonOk);
+            this.Name = "FormMain";
+            this.Text = "DbUpSQLite";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonOk;
+        private System.Windows.Forms.Button buttonAddDatabase;
+        private System.Windows.Forms.TextBox textBoxDatabasePath;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ DbUpSQLite_48/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DbUp;
+
+namespace DbUpSQLite_48
+{
+    public partial class FormMain : Form
+    {
+        string _migrationsPath { get; set; }
+        string _databasePath { get; set; }
+        public string MigrationsPath { get 
+            {
+                return _migrationsPath;
+            } 
+            set {
+                textBox2.Text = value;
+                _migrationsPath = value;
+                if (!System.IO.Directory.Exists(_migrationsPath))
+                {
+                    throw new Exception("Folder of scripts doesn't exists");
+                }
+            } 
+        }
+        public string DatabasePath { get
+            {
+                return _databasePath;
+            } 
+            set
+            {
+                textBoxDatabasePath.Text = value;
+                _databasePath = value;
+            } 
+        }
+        public FormMain()
+        {
+            InitializeComponent();
+            MigrationsPath = $@"D:\repos\HSA_Estoque48\HSA_Estoque48\bin\Release\Migrations";
+            DatabasePath = $@"D:\repos\HSA_Estoque48\HSA_Estoque48\bin\Release\inventory.db";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var connectionString = $@"Data Source={DatabasePath};";
+                var upgrader =
+                    DeployChanges.To
+                    .SQLiteDatabase(connectionString)
+                    .WithScriptsFromFileSystem($@"{MigrationsPath}")
+                    .LogToConsole()
+                    .Build();
+                var result = upgrader.PerformUpgrade();
+                if (!result.Successful)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(result.Error);
+                    Console.ResetColor();
+                    throw new Exception("Folder of scripts doesn't exists");
+                }
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Success!");
+                Console.ResetColor();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+                throw;
+            }
+        }
+
+        private void buttonAddDatabase_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
