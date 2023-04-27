1b0f9e5707bcf43e8f33b41ee2a0873a9de16ecb
edsonlcandido <edson.luiz.candido@gmail.com>
2023-04-26 23:14:56 -0300

    ajustado tamanho da fonte para 11pts

--- DbUpSQLite_48/Form1.Designer.cs
+++ DbUpSQLite_48/Form1.Designer.cs
@@ -37,9 +37,10 @@
             // 
             // buttonOk
             // 
-            this.buttonOk.Location = new System.Drawing.Point(373, 69);
+            this.buttonOk.Location = new System.Drawing.Point(520, 96);
+            this.buttonOk.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.buttonOk.Name = "buttonOk";
-            this.buttonOk.Size = new System.Drawing.Size(116, 23);
+            this.buttonOk.Size = new System.Drawing.Size(174, 32);
             this.buttonOk.TabIndex = 0;
             this.buttonOk.Text = "Execute Migration";
             this.buttonOk.UseVisualStyleBackColor = true;
@@ -47,9 +48,10 @@
             // 
             // buttonAddDatabase
             // 
-            this.buttonAddDatabase.Location = new System.Drawing.Point(12, 12);
+            this.buttonAddDatabase.Location = new System.Drawing.Point(18, 16);
+            this.buttonAddDatabase.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.buttonAddDatabase.Name = "buttonAddDatabase";
-            this.buttonAddDatabase.Size = new System.Drawing.Size(112, 23);
+            this.buttonAddDatabase.Size = new System.Drawing.Size(168, 32);
             this.buttonAddDatabase.TabIndex = 1;
             this.buttonAddDatabase.Text = "Database->";
             this.buttonAddDatabase.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
@@ -58,23 +60,26 @@
             // 
             // textBoxDatabasePath
             // 
-            this.textBoxDatabasePath.Location = new System.Drawing.Point(130, 14);
+            this.textBoxDatabasePath.Location = new System.Drawing.Point(195, 20);
+            this.textBoxDatabasePath.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBoxDatabasePath.Name = "textBoxDatabasePath";
-            this.textBoxDatabasePath.Size = new System.Drawing.Size(359, 20);
+            this.textBoxDatabasePath.Size = new System.Drawing.Size(499, 24);
             this.textBoxDatabasePath.TabIndex = 2;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(130, 43);
+            this.textBox2.Location = new System.Drawing.Point(195, 59);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(359, 20);
+            this.textBox2.Size = new System.Drawing.Size(499, 24);
             this.textBox2.TabIndex = 4;
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(12, 41);
+            this.button3.Location = new System.Drawing.Point(18, 57);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(112, 23);
+            this.button3.Size = new System.Drawing.Size(168, 32);
             this.button3.TabIndex = 3;
             this.button3.Text = "Migrations folder ->";
             this.button3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
@@ -82,14 +87,16 @@
             // 
             // FormMain
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(499, 102);
+            this.ClientSize = new System.Drawing.Size(703, 141);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.textBoxDatabasePath);
             this.Controls.Add(this.buttonAddDatabase);
             this.Controls.Add(this.buttonOk);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "FormMain";
             this.Text = "DbUpSQLite";
             this.ResumeLayout(false);
